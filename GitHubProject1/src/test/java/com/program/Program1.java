c58835cb3c51da45b90006e01384386406a1c40a
HP <HP@LAPTOP-7DH5S9DA>
2025-02-07 09:47:55 +0530

    by mansi

---
+++ GitHubProject1/src/test/java/com/program/Program1.java
@@ -0,0 +1,29 @@
+package com.program;
+
+public class Program1 {
+
+	public void m1() {
+		System.out.println("This is M1 method");
+	}
+	
+	public void m2() {
+		System.out.println("This is M2 method");
+	}
+	
+	public void m3() {
+		System.out.println("This is M3 method");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Program1 p = new Program1();
+		p.m1();
+		p.m2();
+		p.m3();
+			
+		
+	}
+
+}
